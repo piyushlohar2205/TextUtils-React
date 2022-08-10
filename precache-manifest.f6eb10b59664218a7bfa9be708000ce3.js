baf275a990961b44b57af381703e4337b9e27afc
piyushlohar2205 <83692075+piyushlohar2205@users.noreply.github.com>
2022-08-10 13:45:56 +0530

    Updates

---
+++ precache-manifest.f6eb10b59664218a7bfa9be708000ce3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "28e78ac00576d60f3664",
+    "url": "/TextOps/static/css/main.bafc8b19.chunk.css"
+  },
+  {
+    "revision": "28e78ac00576d60f3664",
+    "url": "/TextOps/static/js/main.28e78ac0.chunk.js"
+  },
+  {
+    "revision": "8f2f4211b0891106f089",
+    "url": "/TextOps/static/js/1.8f2f4211.chunk.js"
+  },
+  {
+    "revision": "768fa444867b13b5cb73",
+    "url": "/TextOps/static/js/2.768fa444.chunk.js"
+  },
+  {
+    "revision": "7c7a4f05e63d42b49aad",
+    "url": "/TextOps/static/js/runtime~main.7c7a4f05.js"
+  },
+  {
+    "revision": "56958011777eb1a1f4dd934b92f03f67",
+    "url": "/TextOps/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/BLACKSMITH/precache-manifest.a1922f755fa3959ebd00399b0cab658a.js"
+  "/TextOps/precache-manifest.f6eb10b59664218a7bfa9be708000ce3.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/BLACKSMITH/index.html", {
+workbox.routing.registerNavigationRoute("/TextOps/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
