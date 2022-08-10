cf450974deff376f90ab6a37ee82cb9e1bab15ef
piyushlohar2205 <83692075+piyushlohar2205@users.noreply.github.com>
2022-08-10 11:15:05 +0530

    Updates

---
+++ precache-manifest.a1922f755fa3959ebd00399b0cab658a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "87aeb5354438082c25cc",
+    "url": "/BLACKSMITH/static/css/main.bafc8b19.chunk.css"
+  },
+  {
+    "revision": "87aeb5354438082c25cc",
+    "url": "/BLACKSMITH/static/js/main.87aeb535.chunk.js"
+  },
+  {
+    "revision": "8f2f4211b0891106f089",
+    "url": "/BLACKSMITH/static/js/1.8f2f4211.chunk.js"
+  },
+  {
+    "revision": "cbcdb232e7e31aca1d2c",
+    "url": "/BLACKSMITH/static/js/2.cbcdb232.chunk.js"
+  },
+  {
+    "revision": "a4e27d2c6a983d2dfc02",
+    "url": "/BLACKSMITH/static/js/runtime~main.a4e27d2c.js"
+  },
+  {
+    "revision": "3b1ca182f225eadf9f66de19d65e5d9b",
+    "url": "/BLACKSMITH/index.html"
+  }
+];
